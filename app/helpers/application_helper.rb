e86ac9d17542896c1c52e2cbc954f8c5ad747bd4
Leonam Anjos <leonam.ricardo@gmail.com>
2016-06-10 16:04:23 +0000

    Home controller tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def full_title (page_title = '')
+    base_title = "Contacts Custom Fields Sample App"
+    
+    return base_title if page_title.empty?
+    return page_title + " | " + base_title
+  end
+    
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,9 +3,10 @@ require 'test_helper'
 class HomeControllerTest < ActionController::TestCase
   include Devise::TestHelpers
   
-  test "should get index" do
+  test "should get home#index" do
     get :index
     assert_response :success
+    assert_select "title", "Contacts Custom Fields Sample App"
   end
-
+  
 end
