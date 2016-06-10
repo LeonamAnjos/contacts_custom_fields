8f40423e46465effd9620170e0228344514785e2
Leonam Anjos <leonam.ricardo@gmail.com>
2016-06-10 16:50:23 +0000

    Test Refactoring

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class ContactsControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
-  
   fixtures :users, :contacts
   
   setup do
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  include Devise::TestHelpers
-  
   test "should get home#index" do
     get :index
     assert_response :success
