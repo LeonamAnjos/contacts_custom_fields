7dcb5ec9f3de3032018676bb8deefdd081fa83fd
Leonam Anjos <leonam.ricardo@gmail.com>
2016-06-10 17:51:22 +0000

    Refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
   def full_title (page_title = '')
-    base_title = "Contacts Custom Fields Sample App"
+    base_title = 'Contacts Custom Fields Sample App'
     
     return base_title if page_title.empty?
-    return page_title + " | " + base_title
+    return page_title + ' | ' + base_title
   end
     
 end
